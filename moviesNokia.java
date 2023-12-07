4cb1b63e4e8589c01490687cdc8a94d99c4bc8d7
zafira ibraeva <zafiraibraeva@gmail.com>
2023-12-07 20:34:11 +0100

    added class SearchT that supposed to search for the input title movie

--- moviesNokia.java
+++ moviesNokia.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class Person {
     String name;
@@ -23,10 +25,10 @@ class Movie {
     String title;
     String director;
     int releaseYear;
-    int length; // in minutes
+    String length; // in minutes
     List<String> actors;
 
-    public Movie(String title, String director, int releaseYear, int length, List<String> actors) {
+    public Movie(String title, String director, int releaseYear, String length, List<String> actors) {
         this.title = title;
         this.director = director;
         this.releaseYear = releaseYear;
@@ -47,47 +49,61 @@ class FileModifier {
     }
 }
 
-class DatabaseManager {
-    public static void listMovies(Path filePath){
-        FileReader fread = null;
-        BufferedReader reader = null;
-        
-        try {
-            fread = new FileReader(filePath.toFile());
-            reader = new BufferedReader(fread);
+class ProcessingInputToObjects {
+    public static List<Movie> listMovies(Path filePath) {
+        List<Movie> movies = new ArrayList<>();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath.toFile()))) {
             String line;
-            List<Movie> movies = new ArrayList<>(); 
 
-            try{
-                while ((line = reader.readLine()) != null) {
-                String[] columns = line.split(",");
-                    String title = columns[0];
-                    String director = columns[1];
-                    System.out.println(director);
-                    int releaseYear = Integer.parseInt(columns[2]);
-                    int length = Integer.parseInt(columns[3]);
-                    String[] actorNames = columns[4].split(",");
-                    Movie movie = new Movie(title, director, releaseYear, length, List.of(actorNames));
-                    movies.add(movie);
-                } 
+            while ((line = reader.readLine()) != null) {
+                String[] columns = line.split(";");
+                String title = columns[0];
+                String director = columns[1];
+                int releaseYear = Integer.parseInt(columns[2]);
+                String length = columns[3];
+                String[] actorNames = columns[4].split(",");
+                Movie movie = new Movie(title, director, releaseYear, length, List.of(actorNames));
+                movies.add(movie);
+            }
 
-                for (Movie movie : movies){
-                    System.out.println(movie.title + "; ");
-                    System.out.println(movie.releaseYear + "; ");
-                    System.out.println(movie.director + "; ");
-                }
-            
-            }catch (IOException e){
-                System.err.println("Something is wrong! ");
-            }      
-            
-            
         } catch (FileNotFoundException e) {
             System.err.println("File cannot be opened! ");
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.err.println("Something is wrong! " + e.getMessage());
         }
 
+        return movies;
+    }
+}
 
+class SearchT {
+
+    public static void searchMovies(List<Movie> movies, String userInput) {
+        Pattern pattern = Pattern.compile("-t \"(.*?)\"");
+        Matcher matcher = pattern.matcher(userInput);
+
+        if (matcher.find()) {
+            String titleRegex = matcher.group(1);
+            boolean found = false;
+
+            for (Movie movie : movies) {
+                if (movie.title.matches(titleRegex)) {
+                    System.out.println("Title: " + movie.title);
+                    System.out.println("Release Year: " + movie.releaseYear);
+                    System.out.println("Director: " + movie.director);
+                    System.out.println("Actors: " + String.join(", ", movie.actors));
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                System.out.println("No movies found with the given title: " + titleRegex);
+            }
+        } else {
+            System.out.println("Invalid search criteria. Please provide a title.");
+        }
     }
 }
 
@@ -96,23 +112,37 @@ public class moviesNokia{
     private static List<Movie> movies = new ArrayList<>();
 
     public static void main(String[] args) {
-    
+
         Path filePath = Paths.get("file.txt");
-        
-        FileReader fread = null; 
+
+        FileReader fread = null;
+
+        //String sc = System.console().readLine();
+        //SearchT.searchMovies(movies, sc);
 
         try {
             fread = new FileReader(filePath.toFile());
             System.out.println("Console App has been connected to DataBase Successfully!");
-            DatabaseManager.listMovies(filePath);
+
+            List<Movie> movieList = ProcessingInputToObjects.listMovies(filePath);
+
+            for (Movie movie : movieList) {
+                System.out.println("Title: " + movie.title);
+                System.out.println("Director: " + movie.director);
+                System.out.println("Release Year: " + movie.releaseYear);
+                System.out.println("Length: " + movie.length);
+                System.out.println("Actors: " + String.join(", ", movie.actors));
+                System.out.println();
+
+            }
         } catch (FileNotFoundException f){
             f.printStackTrace();
             System.err.println("Error opening the file: " + f.getMessage());
 
-            System.exit(1); 
-        
+            System.exit(1);
+
         }
 
-        
+
     }
 }
